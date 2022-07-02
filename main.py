996cc698f0be52ae5fe12844d9ad98403a1d1331
Summer <98126132+UmRange@users.noreply.github.com>
2022-07-02 11:44:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from pyparsing import White
+from random import random
 import requests
 import asyncio
 import os
@@ -7,80 +7,123 @@ import time
 from time import sleep
 import sys
 from colorama import Fore
-    		
-    		
-def clear():
-    if os.name == 'nt':
-        os.system('cls')
-    else:
-    	os.system('clear')
-
+import random
 
 green = Fore.LIGHTGREEN_EX
 white = Fore.WHITE
 red = Fore.LIGHTRED_EX
 yellow = Fore.LIGHTYELLOW_EX
 
-bot = discum.Client(token="")
-
-
-
 def sprint(str):
        for c in str + '\n':
         sys.stdout.write(c)
         sys.stdout.flush()
-        time.sleep(4./90)	
+        time.sleep(4./90)
 
-def close_after_fetching(resp, guild_id):
-    if bot.gateway.finishedMemberFetching(guild_id):
-        lenmembersfetched = len(bot.gateway.session.guild(guild_id).members)
-        #print(str(lenmembersfetched) + ' members fetched')
-        bot.gateway.removeCommand({'function': close_after_fetching, 'params': {'guild_id': guild_id}})
-        bot.gateway.close()
 
-def get_members(guild_id, channel_id):
-    bot.gateway.fetchMembers(guild_id, channel_id, keep='all', wait=1)
-    bot.gateway.command({'function': close_after_fetching, 'params': {'guild_id': guild_id}})
-    bot.gateway.run()
-    bot.gateway.resetSession()
-    return bot.gateway.session.guild(guild_id).members 
+def randstr(lenn):
+    alpha = "abcdefghijklmnopqrstuvwxyz0123456789"
+    text = ''
+    for i in range(0, lenn):
+        text += alpha[random.randint(0, len(alpha) - 1)]
+    return text
 
-members = get_members(f'{guild_id}', f'{channel_id}') 
-memberslist = []
 
-for memberID in members:
-    memberslist.append(memberID)
-    print(memberID)
+def mainHeader(token):
+    return {
+        "authorization": token,
+        "accept": "*/*",
+        'accept-encoding': 'gzip, deflate, br',
+        "accept-language": "en-GB",
+        "content-length": "90",
+        "content-type": "application/json",
+        "cookie": f"__cfuid={randstr(43)}; __dcfduid={randstr(32)}; locale=en-US",
+        "origin": "https://discord.com",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/1.0.9003 Chrome/91.0.4472.164 Electron/13.4.0 Safari/537.36",
+        "x-debug-options": "bugReporterEnabled",
+        "x-super-properties": "eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiRGlzY29yZCBDbGllbnQiLCJyZWxlYXNlX2NoYW5uZWwiOiJzdGFibGUiLCJjbGllbnRfdmVyc2lvbiI6IjEuMC45MDAzIiwib3NfdmVyc2lvbiI6IjEwLjAuMjI0NjMiLCJvc19hcmNoIjoieDY0Iiwic3lzdGVtX2xvY2FsZSI6InNrIiwiY2xpZW50X2J1aWxkX251bWJlciI6OTkwMTYsImNsaWVudF9ldmVudF9zb3VyY2UiOm51bGx9"
+    }
 
-f = open('users.txt', "a")
-for element in memberslist:
-    f.write(element + '\n')
-f.close()
 
+def close_after_fetching(token, guild_id):
+	bot = discum.Client(token=token)
+	if bot.gateway.finishedMemberFetching(guild_id):
+		lenmembersfetched = len(bot.gateway.session.guild(guild_id).members)
+		print(str(lenmembersfetched)+' members fetched')
+		bot.gateway.removeCommand({'function': close_after_fetching, 'params': {'guild_id': guild_id}})
+		bot.gateway.close()
+
+def get_members(token, guild_id, channel_id):
+	bot = discum.Client(token=token)
+	bot.gateway.fetchMembers(guild_id, channel_id, keep="all", wait=1)
+	bot.gateway.command({'function': close_after_fetching, 'params': {'guild_id': guild_id}})
+	bot.gateway.run()
+	bot.gateway.resetSession()
+	return bot.gateway.session.guild(guild_id).members
 
 def Menu():
-	print(f"   __                    _____       	")
-	print(f"  /  )             /      /  '  /    	")
-	print(f" /  /o_  _.____ __/    ,-/-,. ./_ __ 	")
-	print(f"/__/</_)(_(_) ((_/_   (_/  (_// </ (_	")
-	print(f"										")
-	print(f"		Developer: Summer 	  		")
-	print(f"										")
-	print(f"[?] Please Select An Option				")
-	print(f"[1] Start Member Scarper				")
-	print(f"[2] Start Mass Dm						")
-	print("											")
-	a=input(f">> "+ white)
-	if a == "1":
-		guild_id =int(input(yellow + '\n[?] Enter the Guild id you would like to scrape: ' + white))
-		sprint(yellow + f"[{white}+{yellow}] Checking Guild ID...")
-		channel_id = int(input(yellow + '\n [?] Enter the Channel id you would like to scarpe: ' + white))
-		sprint(yellow + f"[{white}+{yellow}] Checking Channel ID...")
-		sleep(3)
-		clear()
-		get_members(guild_id, channel_id)
+    	printlogo()
+a=input(f": "+ white)
+if a == "1":
+	guild_id = input(yellow + '\n[?] Enter the Guild id you would like to scrape: ' + white)
+	sprint(yellow + f"[{white}+{yellow}] Checking Guild ID...")
+	channel_id = input(yellow + '\n [?] Enter the Channel id you would like to scarpe: ' + white)
+	sprint(yellow + f"[{white}+{yellow}] Checking Channel ID...")
+	token = input(yellow + '\n [?] What token would you like to scrape everything with?: '+ white)
+	sprint(yellow + f"[{white}+{yellow}] Checking Token...")
+	members = get_members(token, guild_id, channel_id)
+	memberslist = []
+	for memberID in members:
+		memberslist.append(memberID)
+	print(memberID)
+	f = open('users.txt', "a")
+	for element in memberslist:
+		f.write(element + '\n')
+	f.close()
+
+if a == "2":
+	header = mainHeader('yourdiscordtoken')
+payload = {"content": "hello mate!!", "tts": "false"}
+
+response = requests.post(f'https://discord.com/api/v9/channels/targetsuserid/messages', headers=header,json=payload)
+
+if response.ok:
+    	print(f'Successfully Sent a message')
+else:
+    	print(f"Failed to send dm")
+sleep(5)
+Menu()
+
+
+
+def printlogo1():
+    	print(f"""
+			   __                    _____       	
+			  /  )             /      /  '  /    	
+			 /  /o_  _.____ __/    ,-/-,. ./_ __ 	
+			/__/</_)(_(_) ((_/_   (_/  (_// </ (_	
+											
+			Made By Olympus Coding Service		
+				Developer: ImNotSummer 				
+												""")
+
+
+def printlogo():
+    	 print(f"""\n\n __                    _____       	
+			  /  )             /      /  '  /    	
+			 /  /o_  _.____ __/    ,-/-,. ./_ __ 	
+			/__/</_)(_(_) ((_/_   (_/  (_// </ (_	
+											
+			Made By Olympus Coding Service		
+				Developer: ImNotSummer 	  	
+											
+	[?] Please Select An Option			
+	[1] Start Member Scarper			
+	[2] Start Mass Dm						
+												""")
 
 if __name__ == "__main__":
     Menu()
-
-
